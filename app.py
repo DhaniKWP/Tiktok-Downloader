d5805f80ffbcfecc86b69ee9a605a6936c7b4ee6
DhaniKWP <dhanikusuma256@gmail.com>
2025-06-15 23:53:35 +0700

    updated4

--- app.py
+++ app.py
@@ -68,6 +68,5 @@ def download():
         mimetype='video/mp4'
     )
 
-if __name__ == '__main__':
-    print("✅ Flask ready on 0.0.0.0:8080")
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
